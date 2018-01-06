916a3a374e9ac21d051c074525033f044eedb569
pa1511 <peafric@gmail.com>
2018-01-06 11:33:34 +0100

    added properties file

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,11 @@
 package main;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Properties;
 
 import javax.swing.SwingUtilities;
 
@@ -16,9 +19,18 @@ public class Main {
 	
 	
 	public static void main(String[] args) throws UnknownHostException, IOException {
-		Socket socket = new Socket("localhost", 60000);
+
+		//Load properties
+		Properties properties = new Properties();
+		properties.load(new FileInputStream(new File("properties/app.properties")));
+		String server = properties.getProperty("log.server");
+		int port = Integer.parseInt(properties.getProperty("log.port"));
+
+		//Initialize log
+		Socket socket = new Socket(server, port);
 		LoggerProvider.initialize(socket);
-		
+
+		//Start application UI
 		SwingUtilities.invokeLater(()->{
 		ApplicationFrame frame = new ApplicationFrame();
 		frame.addCloseHook(socket);
